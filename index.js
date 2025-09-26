db5b1ce1dc6c90429db178520f57df02fcc8080c
Olawoyin <yustee2017@gmail.com>
2025-09-26 11:30:02 +0100

    fix: use PORT for Railway

--- index.js
+++ index.js
@@ -93,8 +93,3 @@ const server = http.createServer((req, res) => {
   }
 });
 
-// Start the server
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
